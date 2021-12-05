f4502b802c060b954fec2b5fbdf8ce25be368ae1
ci-work <ci@work>
2021-12-05 16:47:32 +0000

    if etl needs it, maybe node needs it

--- src/bn_txns.erl
+++ src/bn_txns.erl
@@ -225,6 +225,74 @@ to_json(T, Opts) ->
     Type = blockchain_txn:json_type(T),
     to_json(Type, T, Opts).
 
+to_json(<<"poc_request_v1">>, T, Opts) ->
+    {ledger, Ledger} = lists:keyfind(ledger, 1, Opts),
+    Json = #{challenger := Challenger} = blockchain_txn:to_json(T, Opts),
+    {ok, ChallengerInfo} = blockchain_ledger_v1:find_gateway_info(?B58_TO_BIN(Challenger), Ledger),
+    ChallengerLoc = blockchain_ledger_gateway_v2:location(ChallengerInfo),
+    Json#{
+        challenger_owner => ?BIN_TO_B58(blockchain_ledger_gateway_v2:owner_address(ChallengerInfo)),
+        challenger_location => ?MAYBE_H3(ChallengerLoc)
+    };
+
+to_json(<<"poc_receipts_v1">>, T, Opts) ->
+    {ledger, Ledger} = lists:keyfind(ledger, 1, Opts),
+    Json = #{challenger := Challenger, path := Path} = blockchain_txn:to_json(T, Opts),
+    UpdateWitness = fun(WitnessJson = #{gateway := Witness}) ->
+        {ok, WitnessInfo} = blockchain_ledger_v1:find_gateway_info(?B58_TO_BIN(Witness), Ledger),
+        WitnessLoc = blockchain_ledger_gateway_v2:location(WitnessInfo),
+        WitnessJson#{
+            owner => ?BIN_TO_B58(blockchain_ledger_gateway_v2:owner_address(WitnessInfo)),
+            location => ?MAYBE_H3(WitnessLoc)
+        }
+    end,
+
+    UpdatePath = fun(PathJson = #{challengee := Challengee, witnesses := Witnesses}) ->
+        {ok, ChallengeeInfo} = blockchain_ledger_v1:find_gateway_info(
+            ?B58_TO_BIN(Challengee),
+            Ledger
+        ),
+        ChallengeeLoc = blockchain_ledger_gateway_v2:location(ChallengeeInfo),
+        PathJson#{
+            challengee_owner => ?BIN_TO_B58(
+                blockchain_ledger_gateway_v2:owner_address(ChallengeeInfo)
+            ),
+            challengee_location => ?MAYBE_H3(ChallengeeLoc),
+            witnesses => [UpdateWitness(W) || W <- Witnesses]
+        }
+    end,
+
+    {ok, ChallengerInfo} = blockchain_ledger_v1:find_gateway_info(?B58_TO_BIN(Challenger), Ledger),
+    ChallengerLoc = blockchain_ledger_gateway_v2:location(ChallengerInfo),
+    Json#{
+        challenger_owner => ?BIN_TO_B58(blockchain_ledger_gateway_v2:owner_address(ChallengerInfo)),
+        challenger_location => ?MAYBE_H3(ChallengerLoc),
+        path => [UpdatePath(E) || E <- Path]
+    };
+
+to_json(<<"state_channel_close_v1">>, T, Opts) ->
+    {ledger, Ledger} = lists:keyfind(ledger, 1, Opts),
+    Json = #{state_channel := SCJson} = blockchain_txn:to_json(T, Opts),
+    UpdateSummary = fun(Summary = #{client := Client}) ->
+        case blockchain_ledger_v1:find_gateway_info(?B58_TO_BIN(Client), Ledger) of
+            {error, _} ->
+                Summary;
+            {ok, ClientInfo} ->
+                blockchain_ledger_v1:find_gateway_info(?B58_TO_BIN(Client), Ledger),
+                ClientLoc = blockchain_ledger_gateway_v2:location(ClientInfo),
+                Summary#{
+                    owner => ?BIN_TO_B58(blockchain_ledger_gateway_v2:owner_address(ClientInfo)),
+                    location => ?MAYBE_H3(ClientLoc)
+                }
+        end
+    end,
+
+    Json#{
+        state_channel => SCJson#{
+            summaries => [UpdateSummary(S) || S <- maps:get(summaries, SCJson)]
+        }
+    };
+
 to_json(<<"rewards_v2">>, T, Opts) ->
     lager:info("in rewards v2"),
     {chain, Chain} = lists:keyfind(chain, 1, Opts),
