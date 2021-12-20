b10a59379ecab08d4c9252ba3db38fdc725d308b
ci-work <ci@work>
2021-12-20 00:23:23 +0000

    add in try catch

--- src/bn_txns.erl
+++ src/bn_txns.erl
@@ -193,7 +193,15 @@ save_transactions(Height, Transactions, Ledger, Chain, #state{
             Hash = blockchain_txn:hash(Txn),
             case application:get_env(blockchain, store_json, false) of
                 true ->
-                    Json = to_json(Txn, JsonOpts),
+                    Json =
+                        try
+                            to_json(Txn, JsonOpts)
+                        catch
+                            _:_ ->
+                                TxnType = blockchain_txn:json_type(Txn),
+                                lager:info("blockchain_txn:to_json catch from ~p", [TxnType]),
+                                blockchain_txn:to_json(Txn, [])
+                        end,
                     ok = rocksdb:batch_put(
                         Batch,
                         JsonCF,
