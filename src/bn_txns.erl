016da6c4a082113c39146abb5649011f118f3ba0
ci-work <ci@work>
2021-12-21 15:42:18 +0000

    fix forced height

--- src/bn_txns.erl
+++ src/bn_txns.erl
@@ -54,7 +54,8 @@ follower_height(#state{db = DB, default = DefaultCF}) ->
             ForcedHeight = application:get_env(blockchain, force_follower_height, 0),
             UseHeight = case ForcedHeight of
                             X when X == 0 -> Height;
-                            X when X /= 0 ->
+                            X when X < Height -> ForcedHeight;
+                            X when X >= 0 ->
                               application:set_env(blockchain, force_follower_height, 0),
                               ForcedHeight
                         end,
