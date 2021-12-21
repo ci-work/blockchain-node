8b52e0be5f3f246ccff694de0fd8bfcb31d9e6aa
ci-work <ci@work>
2021-12-21 00:47:37 +0000

    ..

--- src/bn_txns.erl
+++ src/bn_txns.erl
@@ -49,12 +49,10 @@ init(Args) ->
     end.
 
 follower_height(#state{db = DB, default = DefaultCF}) ->
-    FollowerHeight = case bn_db:get_follower_height(DB, DefaultCF) of
+    case bn_db:get_follower_height(DB, DefaultCF) of
         {ok, Height} -> Height;
         {error, _} = Error -> ?jsonrpc_error(Error)
-    end,
-    Height = application:get_env(blockchain, force_follower_height, FollowerHeight),
-    Height.
+    end.
 
 load_chain(Chain, State = #state{}) ->
     maybe_load_genesis(Chain, State).
